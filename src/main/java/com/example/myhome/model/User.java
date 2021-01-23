dba4db7c4af11eaf309aa89ea8a4730f5d64b8e5
SonGeon <guns716@kpu.ac.kr>
2021-01-23 21:21:15 +0900

    JPA JOIN 어노테이션 수정 / 여러 쿼리를 날리지 않도록 변경

--- src/main/java/com/example/myhome/model/User.java
+++ src/main/java/com/example/myhome/model/User.java
@@ -26,6 +26,8 @@ public class User {
     private List<Role> roles = new ArrayList<>();
 
     //이미 다른 클래스(Board)에서 매핑을 해주었기 떄문에 매팽된 클래스의 변수명을 가지고 mappedBy를 선언한다.
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // fetch = FetchType.LAZY 하나의 칼럼과 여러개의 칼럼이 연결되어 있는 경우 -> 실제 데이터를 사용할때 값을 불러오는 기능
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Board> boards = new ArrayList<>();
 }
--- src/main/java/com/example/myhome/repository/UserRepository.java
+++ src/main/java/com/example/myhome/repository/UserRepository.java
@@ -4,12 +4,17 @@ import com.example.myhome.model.Board;
 import com.example.myhome.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
+    public User findByUsername(String username);
 
+    // model 클래스에 있는 설정을 무시하고 JPA 호출을 할때 JOIN하여 값을 불러온다
+    // 이렇게 값을 설정하지 않으면 값을 불러올때 쿼리를 여러번 날리게 되어 성능상 이슈가 발생하기도 한다.
+    @EntityGraph(attributePaths = {"boards"})
+    public List<User> findAll();
 }
